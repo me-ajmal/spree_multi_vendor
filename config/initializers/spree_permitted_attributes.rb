d7582939b45cdad6b52ef15b9f00afbf5c3302ba
Ajmal-hassan <ajmalhassan651@gmail.com>
2020-10-30 09:29:26 +0500

    permited attributes

--- config/initializers/spree_permitted_attributes.rb
+++ config/initializers/spree_permitted_attributes.rb
@@ -4,7 +4,7 @@ module Spree
 
     mattr_reader *ATTRIBUTES
 
-    @@vendor_attributes = [:name, :about_us, :contact_us, :notification_email]
+    @@vendor_attributes = [:name, :about_us, :contact_us, :notification_email, :age, :category_id, :gender, :country, :paypal_id]
     @@vendor_attributes << :image if Spree.version.to_f >= 3.6
   end
 end
